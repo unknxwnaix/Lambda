9aa0a96f9d59d31862e6800576d80b17df82b49e
unknxwnaix <113522344+unknxwnaix@users.noreply.github.com>
2024-03-20 14:54:07 +0300

    Initial Commit

---
+++ Lambda/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lambda
+//
+//  Created by Maxim Dmitrochenko on 20.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
